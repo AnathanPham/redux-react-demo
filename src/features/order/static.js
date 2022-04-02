0d878d5342e27c356b502be07f61b387a1e9e79a
huangbangsheng <huangbangsheng@joyy.com>
2022-04-02 17:32:40 +0800

    feat: 高性能证明

--- src/features/order/static.js
+++ src/features/order/static.js
@@ -3,11 +3,29 @@ export const OrderStatusMap = {
   UNPAY: "unpay",
 };
 
+const SelectorLogStatusMap = {
+  OPEN: "open",
+  CLOSE: "close",
+};
 export const myConsole = {
-  component:(text)=>{
-    console.log(`%c ${text} render`,'color:#067413;')
+  log: (text) => {
+    console.log(`%c ${text}`, "color:#f00;");
   },
-  selector:(text)=>{
-    console.log(`%c ${text} - selector run`,'color:#5707cf;')
+
+  component: (text) => {
+    console.log(`%c ${text} render`, "color:#067413;");
   },
-}
+
+  selector: function (text) {
+    if (this.selectorLogStatus === SelectorLogStatusMap.OPEN) {
+      console.log(`%c ${text} - selector run`, "color:#5707cf;");
+    }
+  },
+  selectorOpen: function () {
+    this.selectorLogStatus = SelectorLogStatusMap.OPEN;
+  },
+  selectorClose: function () {
+    this.selectorLogStatus = SelectorLogStatusMap.CLOSE;
+  },
+  selectorLogStatus: SelectorLogStatusMap.CLOSE,
+};
